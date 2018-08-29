9c0d25df7901010c3afb9377f1187cb60712eb08
Bharat Kunwar <b.kunwar@gmail.com>
2018-08-29 19:39:05 +0000

    Demo version

---
+++ api.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+from flask import request
+
+app = Flask(__name__)
+api = Api(app)
+
+import main
+
+
+@api.route('/game')
+class HelloWorld(Resource):
+    def get(self):
+        return {'board': main.game.to_json()}
+
+    def put(self):
+        x = int(request.form['x']) % main.n
+        y = int(request.form['y']) % main.m
+        main.game.board[x][y].hit = True
+        print(main.game)
+        pass
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 BATTLESHIP
 n * m
 '''
+import random
 
 n = 10
 m = 10
@@ -30,11 +31,41 @@ class Cell(object):
 class Game(object):
     def __init__(self, n, m, ships):
         board = []
+        n_cells = n*m
         for i in range(n):
             column = []
             for j in range(m):
                 column.append(Cell())
             board.append(column)
+        list_of_ships = list()
+        for ship_type in ships:
+            for ship in range(int(n_cells*ship_type['percent'])):
+                list_of_ships.append(ship_type['length'])
+        random.shuffle(list_of_ships)
+        for length in list_of_ships:
+            while True:
+                i,j = random.choice(range(n)), random.choice(range(m))
+                orient = random.choice(range(2))
+                free = True
+                # Brute force and determine if the slot of that length is free
+                for k in range(length):
+                    if orient == 0:
+                        if board[(i+k)%n][j].has_ship:
+                            free = False
+                    if orient == 1:
+                        if board[i][(j+k)%m].has_ship:
+                            free = False
+                # Populate those cells with a ship and break loop
+                if free == True:
+                    for k in range(length):
+                        if orient == 0:
+                            board[(i+k)%n][j].has_ship = True
+                        if orient == 1:
+                            board[i][(j+k)%m].has_ship = True
+                    break
+            print (length)
+
+                        
         self.board = board
 
     def __repr__(self):
@@ -45,7 +76,28 @@ class Game(object):
             r += '\n'
         return r[:-1]
 
+    def to_json(self):
+        b = []
+        for row in self.board:
+            column = []
+            for c in row:
+                v = 0 # nothjing
+                if c.hit:
+                    if c.has_ship:
+                        v = 2 # hit a ship
+                    else:
+                        v = 1 # missed
+                column.append(v)
+            b.append(column)
+        return b
 
 game = Game(n, m, ships)
-game.board[1][1].hit = True
+#game.board[1][1].hit = True
+
+'''
+for i in range(30):
+    x, y = random.randint(0, n-1), random.randint(0, m-1)
+    game.board[x][y].hit = True
+'''
+
 print(game)
